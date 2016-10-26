b69b51a2abdbd051fbdb210cd549e8c37bfd7828
Ryan "archaen" Pando <vretha@gmail.com>
2016-10-26 09:42:45 -0600

    deploy

--- src/js/firebase.js
+++ src/js/firebase.js
@@ -1,9 +1,10 @@
 
      // Initialize Firebase
      var config = {
-       apiKey: "AIzaSyApgf6u4lVoiaxJNZLFs7PnMGlkdIur5Kg",
-       authDomain: "awesome-locks.firebaseapp.com",
-       databaseURL: "https://awesome-locks.firebaseio.com",
-       storageBucket: "awesome-locks.appspot.com",
-     };
+        apiKey: "AIzaSyApgf6u4lVoiaxJNZLFs7PnMGlkdIur5Kg",
+        authDomain: "awesome-locks.firebaseapp.com",
+        databaseURL: "https://awesome-locks.firebaseio.com",
+        storageBucket: "awesome-locks.appspot.com",
+        messagingSenderId: "633767537518"
+      };
      firebase.initializeApp(config);
